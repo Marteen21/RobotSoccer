22531831d98768b623b6f33a5acd0e3352fffd22
Marteen21 <znmarteen21@gmail.com>
2016-03-09 11:55:56 +0100

    Testing GUI capabilities

---
+++ testfigure.m
@@ -0,0 +1,89 @@
+function varargout = testfigure(varargin)
+% TESTFIGURE MATLAB code for testfigure.fig
+%      TESTFIGURE, by itself, creates a new TESTFIGURE or raises the existing
+%      singleton*.
+%
+%      H = TESTFIGURE returns the handle to a new TESTFIGURE or the handle to
+%      the existing singleton*.
+%
+%      TESTFIGURE('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in TESTFIGURE.M with the given input arguments.
+%
+%      TESTFIGURE('Property','Value',...) creates a new TESTFIGURE or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before testfigure_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to testfigure_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help testfigure
+
+% Last Modified by GUIDE v2.5 09-Mar-2016 11:21:28
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @testfigure_OpeningFcn, ...
+                   'gui_OutputFcn',  @testfigure_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before testfigure is made visible.
+function testfigure_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to testfigure (see VARARGIN)
+
+% Choose default command line output for testfigure
+handles.output = hObject;
+
+%Magic happens here
+handles.peaks = peaks(35);
+handles.membrane = membrane;
+[x, y] = meshgrid(-8:0.5:8);
+r = sqrt(x.^2+y.^2) + eps;
+sinc = sin(r)./r;
+handles.sinc = sinc;
+handles.current_data = handles.peaks;
+surf (handles.current_data);
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes testfigure wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = testfigure_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes on button press in pushbutton1.
+function pushbutton1_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
