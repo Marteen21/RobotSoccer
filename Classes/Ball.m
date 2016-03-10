65385a683f0070f0d57ec3e5bc2a5ba19bbab1a4
pifej <peter.m93@gmail.com>
2016-03-10 21:19:44 +0100

    Multiple constructors with nargin

--- Classes/Ball.m
+++ Classes/Ball.m
@@ -8,14 +8,30 @@ classdef Ball
     end
     
     methods
-        function obj = Ball(aX,aY)
-            obj.Position = Vector2(aX,aY)
-        end
-        
         function obj = Ball(aX,aY,fric)
-            obj.Position = Vector2(aX,aY);
-            obj.Friction = fric;
+            
+        switch nargin
+            
+            case 2
+                obj.Position = Vector2(aX,aY);
+                
+            case 3
+                obj.Position = Vector2(aX,aY);
+                obj.Friction = fric;
+                ...
+            otherwise
+                error('Ball class: Number of input arguments must be 2 or 3');
+            
         end
+            
+%         function obj = Ball(aX,aY)
+%             obj.Position = Vector2(aX,aY)
+%         end
+%         
+%         function obj = Ball(aX,aY,fric)
+%             obj.Position = Vector2(aX,aY);
+%             obj.Friction = fric;
+%         end
         
         
     end
