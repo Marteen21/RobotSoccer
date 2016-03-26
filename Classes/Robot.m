a7caea3976b722dfd55475bdd7e309e42befc5fe
pifej <peter.m93@gmail.com>
2016-03-26 11:39:05 +0100

    Robot (as a ball) radius added

--- Classes/Robot.m
+++ Classes/Robot.m
@@ -5,12 +5,14 @@ classdef Robot
         Position;       %Vector2
         Orientation;    %Vector2
         Simulation;     %SimulationData
+        Radius;         %Radius of the simulated robot
     end
     
     methods
-        function this = Robot(aX, aY, aO)
+        function this = Robot(aX, aY, aO, r)
             this.Position = Vector2(aX, aY);
             this.Orientation = aO;
+            this.Radius = r;
         end        
     end
 end
