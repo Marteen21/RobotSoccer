5568498777bc35607b290f260b15648b8be381a5
pifej <peter.m93@gmail.com>
2016-03-30 12:28:21 +0200

    -Added Rodriguez.m

---
+++ Functions/Assistant/Rodriguez.m
@@ -0,0 +1,8 @@
+function [ Rotate ] = Rodriguez( theta )
+%RODRIGUEZ Summary of this function goes here
+%   Rotate a vector with a given theta
+Rotate = [cos(theta) -sin(theta);
+          sin(theta) cos(theta)]
+
+end
+
