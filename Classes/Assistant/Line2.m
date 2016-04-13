dfcf722d2dd36b4da803a9f524e56b8973ff59f1
Marteen21 <znmarteen21@gmail.com>
2016-04-13 13:53:03 +0200

    Added function that determinates the exact time of the collision (sub sample time)

--- Classes/Assistant/Line2.m
+++ Classes/Assistant/Line2.m
@@ -7,26 +7,61 @@ classdef Line2
     end
     
     methods
-        function this = Line2(op1,op2,op3,op4)
+        function this = Line2(op1,op2,op3,op4,op5,op6)
             switch nargin
                 
                 case 2
-                    this.m = op1;
-                    this.b = op2;
+                    m = op1;
+                    b = op2;
+                    
+                    this.m = m;
+                    this.b = b;
                 case 4
-                    this.m = (op1-op2)/(op3-op4);
-                    this.b = -1*this.m*op3+op1;   
-                 ...
+                    distance0 = op1;
+                    distanceT = op2;
+                    time0 = op3;
+                    timeT = op4;
+                    
+                    this.m = (distance0-distanceT)/(time0-timeT);
+                    this.b = -1*this.m*time0+distance0;
+                case 6
+                    Ap0 = op1;
+                    Bp0 = op2;
+                    ApT = op3;
+                    BpT = op4;
+                    time0 = op5;
+                    timeT = op6;
+                    
+                    distance0 = Ap0.Distance(Bp0);
+                    temp1 = sign(Ap0.X-Bp0.X)*sign(ApT.X-BpT.X);
+                    temp2 = sign(Ap0.Y-Bp0.Y)*sign(ApT.Y-BpT.Y);
+                    if(temp1+temp2 < 0)
+                        distanceT = -1*ApT.Distance(BpT);
+                    else
+                        distanceT = ApT.Distance(BpT);
+                    end
+                    
+                    this.m = (distance0-distanceT)/(time0-timeT);
+                    this.b = -1*this.m*time0+distance0;
+                    ...
                 otherwise
-                error('Ball class: Number of input arguments must be 2 or 3');
-
+                error('Ball class: Number of input arguments must be 2 or 4 or 6');
+                
             end
         end
-        function f = Lequ(this)
+        function f = equation(this)
             f = @(t) this.b+this.m*t;
+        end
+        function f = absequation(this)
+            f = @(t) abs(this.b+this.m*t);
         end
         function result = minus(this,other)
             result = Line2(this.m-other.m,(this.b-other.b));
+        end
+        function results = TfromD(this,d)
+            t1 = (d-this.b)/(this.m);
+            t2 = (-d-this.b)/(this.m);
+            results = [t1;t2];
         end
     end
     
--- Tester/testcollision.m
+++ Tester/testcollision.m
@@ -11,48 +11,49 @@ M = [Av.D2NVector().RowForm(),dot(Av.D2NVector().RowForm(),Ap.RowForm());
 
 %%
 
-b0 = 0;
-b1 = -1;
-c0 = 5;
-c1 = 1;
-f = @(x) b0-b1*x;
-f2 = @(x) c0-c1*x;
-d0 = 2;
-d1 = 5/2;
-f3 = @(x) -2*x+8;
-%%
-% ezplot( f, 0, 5 )
-% hold on
-% ezplot ( f2, 0, 5)
-% ezplot (f3,0,5)
-% grid on
-% ezplot (@(x) 0,0,5)
+% b0 = 0;
+% b1 = -1;
+% c0 = 5;
+% c1 = 1;
+% f = @(x) b0-b1*x;
+% f2 = @(x) c0-c1*x;
+% d0 = 2;
+% d1 = 5/2;
+% f3 = @(x) -2*x+8;
+% %%
+% % ezplot( f, 0, 5 )
+% % hold on
+% % ezplot ( f2, 0, 5)
+% % ezplot (f3,0,5)
+% % grid on
+% % ezplot (@(x) 0,0,5)
+% % 
+% % %%2 2d shit
 % 
-% %%2 2d shit
+% x0a=0;
+% vxa=1;
+% x0b=5;
+% vxb=-1;
+% 
+% Ax = Line2(vxa,x0a);
+% Bx = Line2(vxb,x0b);
+% Cx = Ax-Bx;
+% 
+% y0a=0;
+% vya=1;
+% y0b=0;
+% vyb=1;
+% 
+% Ay = Line2(vya,y0a);
+% By = Line2(vyb,y0b);
+% Cy = Ay-By;
+% temp = (Ap+Av.*T);
 
-x0a=0;
-vxa=1;
-x0b=5;
-vxb=-1;
-
-Ax = Line2(vxa,x0a);
-Bx = Line2(vxb,x0b);
-Cx = Ax-Bx;
-
-y0a=0;
-vya=1;
-y0b=0;
-vyb=1;
-
-Ay = Line2(vya,y0a);
-By = Line2(vyb,y0b);
-Cy = Ay-By;
-temp = (Ap+Av.*T);
-
-D = Line2(Ap.Distance(Bp),temp.Distance(Bp+Bv.*T),0,T);
+D = Line2(Ap,Bp,Ap+Av.*T,Bp+Bv.*T,0,T);
 % ezplot (Cx.Lequ,0,5)
 
-hold on
 % ezplot (Cy.Lequ,0,5)
-ezplot (D.Lequ,0,5)
+ezplot (D.absequation,0,5)
 grid on
+
+D.TfromD(2)
