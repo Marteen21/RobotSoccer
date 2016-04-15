3dc484c0253cdac71237e7031758831949f7c78d
pifej <peter.m93@gmail.com>
2016-04-15 21:42:33 +0200

    - add

--- Functions/Assistant/Intializer.m
+++ Functions/Assistant/Intializer.m
@@ -1,4 +1,7 @@
 disp('Setting global static valuables');
 SimulationData.friction(0);
 SimulationData.sampleTime(0.1);
-Environment.CreatField(100,100,[0 50],20)
+Environment.set_xLim(100);
+Environment.set_yLim(100);
+Environment.set_goalPos([0 50]);
+Environment.set_goalLength(20);
