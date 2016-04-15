2df9a421753231a27874c87f071cd73bf7f5030d
Marteen21 <znmarteen21@gmail.com>
2016-04-15 15:25:35 +0200

    -Added TotalReflection

--- Classes/Assistant/Vector2.m
+++ Classes/Assistant/Vector2.m
@@ -9,15 +9,23 @@ classdef Vector2
     end
     
     methods
-        %Class constructor
+        %% Class constructor
         function this = Vector2(aX,aY)
-            this.X = aX;
-            this.Y = aY;
+            if nargin == 1
+                this.X = aX(1);
+                this.Y = aX(2);
+            elseif nargin == 2
+                this.X = aX;
+                this.Y = aY;
+            else
+                error('arg error');
+            end
         end
         %Returns the distance between 2 vectors
         function d = Distance(this,other)
             d = sqrt((other.X-this.X)^2+(other.Y-this.Y)^2);
         end
+        %% Return as matlab vectors
         %Return the class as a Matlab 1x2 Matrix
         function v = RowForm(this)
             v = [this.X,this.Y];
@@ -26,31 +34,53 @@ classdef Vector2
         function v = ColumnForm(this)
             v = [this.X;this.Y];
         end
+        %% Rodriguez shit
         %return the incident angle
         function alpha = CIncidentAngle(this,other)
             alpha = (acos(dot(this.ColumnForm,other.ColumnForm)/(norm(this.ColumnForm)*norm(other.ColumnForm))));
         end
-        
         function alpha = SIncidentAngle(this,other)
             alpha = (asin(cross([this.ColumnForm;0],[other.ColumnForm;0])/(norm(this.ColumnForm)*norm(other.ColumnForm))));
         end
-        
         function theta = RodriguezRot(this,other)
             alpha = SIncidentAngle(this,other);
             theta = sign(alpha(3))*(pi-2*abs(alpha(3)));
         end
+        %% Functions
         function nvector = D2NVector(this)
             nvector = Vector2(this.Y,-this.X);
         end
+        %Gives back the reflected vector from wall
+        function result = TotalReflectionFrom(this, wall)
+            e = wall.RowForm()/norm(wall.RowFom());
+            n1 = this.RowForm()/norm(this.RowForm());
+            n2 = 2*(dot(n1,e))*e-n1;
+            result = Vector2(n2*norm(this.RowForm()));
+        end
+        %% Operators -,+,.*
         function result = minus (this, other)
-            result = Vector2(this.X-other.X, this.Y-other.Y);
+            if isa(other,'Vector2')
+                result = Vector2(this.X-other.X, this.Y-other.Y);
+            else
+                error('The - operator is not defined for this two classes.');
+            end
         end
         function result = plus (this, other)
-            result = Vector2(this.X+other.X, this.Y+other.Y);
+            if isa(other,'Vector2')
+                result = Vector2(this.X+other.X, this.Y+other.Y);
+            else
+                error('This operator is not defined for this two classes.');
+            end
         end
         function result = times(this, c)    %Times a constant
-           result.X = this.X*c;
-           result.Y = this.Y*c;
+            if (isnumeric(c))
+                result = Vector2(this.X*c,this.Y*c);
+                %elseif isa(c,'Vector2')
+                %
+            else
+                error('This operator is not defined for this two classes.');
+            end
+            
         end
     end
 end
--- Tester/testcollision.m
+++ Tester/testcollision.m
@@ -10,50 +10,16 @@ M = [Av.D2NVector().RowForm(),dot(Av.D2NVector().RowForm(),Ap.RowForm());
     Bv.D2NVector().RowForm(),dot(Bv.D2NVector().RowForm(),Bp.RowForm())]
 
 %%
-
-% b0 = 0;
-% b1 = -1;
-% c0 = 5;
-% c1 = 1;
-% f = @(x) b0-b1*x;
-% f2 = @(x) c0-c1*x;
-% d0 = 2;
-% d1 = 5/2;
-% f3 = @(x) -2*x+8;
-% %%
-% % ezplot( f, 0, 5 )
-% % hold on
-% % ezplot ( f2, 0, 5)
-% % ezplot (f3,0,5)
-% % grid on
-% % ezplot (@(x) 0,0,5)
-% % 
-% % %%2 2d shit
-% 
-% x0a=0;
-% vxa=1;
-% x0b=5;
-% vxb=-1;
-% 
-% Ax = Line2(vxa,x0a);
-% Bx = Line2(vxb,x0b);
-% Cx = Ax-Bx;
-% 
-% y0a=0;
-% vya=1;
-% y0b=0;
-% vyb=1;
-% 
-% Ay = Line2(vya,y0a);
-% By = Line2(vyb,y0b);
-% Cy = Ay-By;
-% temp = (Ap+Av.*T);
-
 D = Line2(Ap,Bp,Ap+Av.*T,Bp+Bv.*T,0,T);
-% ezplot (Cx.Lequ,0,5)
-
-% ezplot (Cy.Lequ,0,5)
 ezplot (D.absequation,0,5)
 grid on
 
 D.TfromD(2)
+
+n1 = [1,1];
+n2 = [0,1];
+n1 = n1/norm(n1);
+e = [1,-1];
+e = e/norm(e);
+2*(dot(n1,e))*e-n1
+2*(dot(n2,e))*e-n2
