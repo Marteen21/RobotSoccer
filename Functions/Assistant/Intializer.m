56cf2b21bc2e66bb21d13034ca74c85d61d2d9c6
pifej <peter.m93@gmail.com>
2016-04-15 21:45:46 +0200

    -delete

--- Functions/Assistant/Intializer.m
+++ Functions/Assistant/Intializer.m
@@ -1,7 +1,7 @@
 disp('Setting global static valuables');
 SimulationData.friction(0);
 SimulationData.sampleTime(0.1);
-Environment.set_xLim(100);
-Environment.set_yLim(100);
-Environment.set_goalPos([0 50]);
-Environment.set_goalLength(20);
+Environment.xLim(100);
+Environment.yLim(100);
+Environment.goalPos([0 50]);
+Environment.goalLength(20);
