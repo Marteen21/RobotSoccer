6f0c314f1cfe4b9c438b445c6fe6aa9da7a7246e
pifej <peter.m93@gmail.com>
2016-04-16 12:59:56 +0200

    -goalPos new def

--- Functions/Assistant/Intializer.m
+++ Functions/Assistant/Intializer.m
@@ -3,5 +3,5 @@ SimulationData.friction(0);
 SimulationData.sampleTime(0.1);
 Environment.xLim(100);
 Environment.yLim(100);
-Environment.goalPos([0 50]);
+Environment.goalPos(0,50);
 Environment.goalLength(20);
