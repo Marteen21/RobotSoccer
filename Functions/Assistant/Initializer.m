d137ca1926fecb77b74d32408600647281e3361d
Marteen21 <znmarteen21@gmail.com>
2016-04-17 00:39:13 +0200

    Small changes in field size

--- Functions/Assistant/Initializer.m
+++ Functions/Assistant/Initializer.m
@@ -1,7 +1,7 @@
 disp('Setting global static valuables');
 SimulationData.friction(0);
 SimulationData.sampleTime(0.1);
-Environment.xLim(100);
-Environment.yLim(100);
+Environment.xLim(115);
+Environment.yLim(74);
 Environment.goalPos(0,50);
 Environment.goalLength(20);
--- Functions/Collision.m
+++ Functions/Collision.m
@@ -4,10 +4,7 @@ c = [myState];
 plot (0,0,'o');
 plot (100,100,'o');
 hold on
-for i = 1:6000
+for i = 1:3000
     c(end+1) = c(end).NextState();
     plot(c(end).ball.Position.X,c(end).ball.Position.Y,'o');
-    if(mod(i,100)==0)
-        disp(c(end).ball.Position.X);
-    end
 end
