6bfee0dba12aff2f3459e24710a48f90ac9068f7
pifej <peter.m93@gmail.com>
2016-04-16 16:02:39 +0200

    -can draw the goals

--- Tester/testmain.m
+++ Tester/testmain.m
@@ -5,4 +5,9 @@ viscircles([robot1.Position.X robot1.Position.Y],robot1.Radius)
 hold on
 plot(Environment.goalPos.X,Environment.goalPos.Y + (Environment.goalLength/2),'X');
 plot(Environment.goalPos.X,Environment.goalPos.Y - (Environment.goalLength/2),'X');
+goalPosT2 = ([Environment.goalPos.X Environment.goalPos.Y]*Environment.FieldTransform) + ([Environment.xLim Environment.yLim 0]);
+goalPosT2 = Vector2(goalPosT2(1),goalPosT2(2))
+plot(goalPosT2.X,goalPosT2.Y + (Environment.goalLength/2),'X');
+plot(goalPosT2.X,goalPosT2.Y - (Environment.goalLength/2),'X');
+
 
