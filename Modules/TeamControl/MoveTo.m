048c6d3833d9b5b9108cdcc80d631087252c84f5
pifej <peter.m93@gmail.com>
2016-11-24 23:02:50 +0100

    "-Robot speed point at the desired target"

---
+++ Modules/TeamControl/MoveTo.m
@@ -0,0 +1,16 @@
+function desiredSpeed = MoveTo( robot, Target )
+%MOVETO Summary of this function goes here
+%   Moving to the desired target
+    
+    targetSpeed = robot.Position-(Target);
+    targetSpeed = Vector2(targetSpeed.RowForm()/norm(targetSpeed.RowForm())* 15);
+    diffSpeed = robot.Simulation.Speed-targetSpeed;
+    diffSpeed = Vector2(diffSpeed.RowForm()/norm(diffSpeed.RowForm())* SimulationData.sampleTime* 10);
+    desiredSpeedTemp = robot.Simulation.Speed + diffSpeed;
+    if norm(desiredSpeedTemp.RowForm()) >= 15
+        desiredSpeed = Vector2(robot.Simulation.Speed.RowForm()/ norm(robot.Simulation.Speed.RowForm())*15);
+    end
+
+
+end
+
