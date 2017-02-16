a1050f25cdd166fb4ad77cd39e924791a4b460d2
root <root@peter-Inspiron-N5110>
2017-02-16 18:37:50 +0100

    -diff changing

--- Modules/TeamControl/MoveTo.m
+++ Modules/TeamControl/MoveTo.m
@@ -2,6 +2,7 @@ function desiredSpeed = MoveTo( robot, Target )
 %MOVETO Summary of this function goes here
 %   Moving to the desired target
     TargetOri = Target-robot.Position;
+    TempSpeedRobot = robot.Simulation.Speed;
     %if the angle between the robot orientation and the direction the
     %target is greater than 40° the robot first change it's orientation and
     %after that it moves to the target.
@@ -31,28 +32,29 @@ function desiredSpeed = MoveTo( robot, Target )
         robot.Simulation.Speed = Vector2(0,0);
         %robot.Orientation = Vector2(robot.Orientation.RowForm()*Rodriguez(-1*Control(1,2)));
         %robot.Orientation = Vector2(TargetOri.RowForm()/norm(TargetOri.RowForm()));
-        robot.Orientation = TargetOri;
+        %robot.Orientation = TargetOri;
         
         %if (cross0(robot.Orientation,TargetOri))
-           targetSpeed = robot.Position-(Target);
-           targetSpeed = Vector2(targetSpeed.RowForm()/norm(targetSpeed.RowForm())* 15);
-           diffSpeed = robot.Simulation.Speed-targetSpeed;
-           diffSpeed = Vector2(diffSpeed.RowForm()/norm(diffSpeed.RowForm())* SimulationData.sampleTime* 10);
-           desiredSpeedTemp = robot.Simulation.Speed + diffSpeed;
-           if (norm(desiredSpeedTemp.RowForm()) >= 15)
-               desiredSpeed = Vector2(desiredSpeedTemp.RowForm()/ norm(desiredSpeedTemp.RowForm())*15);
-           else
-               desiredSpeed = Vector2(desiredSpeedTemp.RowForm());
-           end
-           
-           if (cross([robot.Simulation.Speed.X,robot.Simulation.Speed.Y,0],[robot.Orientation.X,robot.Orientation.Y,0])==0)
-               if norm(robot.Orientation.RowForm()) >= 15
-                   robot.Simulation.Speed = Vector2(robot.Orientation.RowForm()/ norm(robot.Orientation.RowForm())*15);
-               end
-           else
-               %robot.Orientation = Vector2(-1*(targetSpeed.RowForm())/norm(targetSpeed.RowForm()));
-               robot.Simulation.Speed = Vector2(robot.Orientation.RowForm()/ norm(robot.Orientation.RowForm()));
-           end
+            targetSpeed = robot.Position-(Target);
+            targetSpeed = Vector2(targetSpeed.RowForm()/norm(targetSpeed.RowForm())* 15);
+            diffSpeed = robot.Simulation.Speed-targetSpeed;
+            diffSpeed = Vector2(diffSpeed.RowForm()/norm(diffSpeed.RowForm())* SimulationData.sampleTime* 10);
+            desiredSpeedTemp = robot.Simulation.Speed + diffSpeed;
+            robot.Simulation.Speed = TempSpeedRobot;
+            if (norm(desiredSpeedTemp.RowForm()) >= 15)
+                desiredSpeed = Vector2(desiredSpeedTemp.RowForm()/ norm(desiredSpeedTemp.RowForm())*15);
+            else
+                desiredSpeed = Vector2(desiredSpeedTemp.RowForm());
+            end
+            
+            if (cross([robot.Simulation.Speed.X,robot.Simulation.Speed.Y,0],[robot.Orientation.X,robot.Orientation.Y,0])==0)
+                if norm(robot.Simulation.Speed.RowForm()) >= 15
+                    robot.Simulation.Speed = Vector2(robot.Simulation.Speed.RowForm()/ norm(robot.Simulation.Speed.RowForm())*15);
+                end
+            else
+                %robot.Orientation = Vector2(-1*(targetSpeed.RowForm())/norm(targetSpeed.RowForm()));
+                robot.Orientation = Vector2(robot.Simulation.Speed.RowForm()/ norm(robot.Simulation.Speed.RowForm()));
+            end
         %end
         
     end
