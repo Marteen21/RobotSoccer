a661bb725969fc939eacf118144cfc3e2b8bbb69
pifej <peter.m93@gmail.com>
2017-04-25 16:38:04 +0200

    "-find the robot in the potField (which cell contains the robot)"

---
+++ Modules/TeamControl/TeamA/locateMyRobot.m
@@ -0,0 +1,43 @@
+function [robX, robY] = locateMyRobot(Field,Robot,Target)
+    if nargin < 3
+        posX = Robot.Position.X;
+        posY = Robot.Position.Y;
+        xStepCell = Field(1);
+        xStep = xStepCell{1,1}{1,1};
+        yStepCell = Field(2);
+        yStep = yStepCell{1,1}{1,1};
+
+        robX = nan;
+        robY = nan;
+
+        for i =1 : size(xStep,2)-1
+            for j =1 : size(yStep,2)-1
+                if (posX > xStep(1,i) && posX < xStep(1,i+1) && ...
+                        posY > yStep(1,j) && posY < yStep(1,j+1))
+                    robX = i;
+                    robY = j;
+                end
+            end
+        end
+    else
+        posX = Robot.X;
+        posY = Robot.Y;
+        xStepCell = Field(1);
+        xStep = xStepCell{1,1}{1,1};
+        yStepCell = Field(2);
+        yStep = yStepCell{1,1}{1,1};
+
+        robX = nan;
+        robY = nan;
+
+        for i =1 : size(xStep,2)-1
+            for j =1 : size(yStep,2)-1
+                if (posX > xStep(1,i) && posX < xStep(1,i+1) && ...
+                        posY > yStep(1,j) && posY < yStep(1,j+1))
+                    robX = i;
+                    robY = j;
+                end
+            end
+        end
+    end
+end
