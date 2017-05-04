131f77297bc34ff1161cc9e623ffbe7a5f43effc
pifej <peter.m93@gmail.com>
2017-05-04 13:39:53 +0200

    "-potField buildup now works properly"

--- Modules/TeamControl/TeamA/buildUpPotField.m
+++ Modules/TeamControl/TeamA/buildUpPotField.m
@@ -4,30 +4,31 @@ function [potField, robotIndexes] = buildUpPotField(State, teamMemberOwn, Target
     potField{1}(1,1) = nan;
     robotIndexes{1,1} = [0 0;];
     robotIndexes{1,2} = [0 0;];
-    potObst = [];
+    potObst{1} = [];
     sigma = sqrt(10);  %Sulytenyezo a potencial ter szamitasanal,
                   %pontos beállítása kérdéses;
     robotValue = 8;
     targetValue = -1;
     %Selecting the given team members
     Obstacle = [teamMemberOwn.empty];
+    potStep = (teamMemberOwn(1).Radius);
     
     
     
-    for i = 1:length(teamMemberOwn)
+    for i = 1:length(teamMemberOwn)-1
           Orient = atan2(Target{i}.Y-teamMemberOwn(i).Position.Y,Target{i}.X-teamMemberOwn(i).Position.X);
           if (Orient < pi/2 && Orient > -pi/2)
-              xStep{1,i} = (teamMemberOwn(i).Position.X-teamMemberOwn(i).Radius):2*(teamMemberOwn(i).Radius):(Target{i}.X+teamMemberOwn(i).Radius);
+              xStep{1,i} = (teamMemberOwn(i).Position.X-teamMemberOwn(i).Radius):potStep:(Target{i}.X+teamMemberOwn(i).Radius);
               if (xStep{1,i}(1,end) < Target{i}.X+teamMemberOwn(i).Radius)
-                  xStep{1,i}(1,end+1) = xStep{1,i}(1,end)+2*(teamMemberOwn(i).Radius);
+                  xStep{1,i}(1,end+1) = xStep{1,i}(1,end)+potStep;
               end
-              yStep{1,i} = 0:2*(teamMemberOwn(i).Radius):Environment.yLim;
+              yStep{1,i} = 0:potStep:Environment.yLim;
           else
-              xStep{1,i} = (teamMemberOwn(i).Position.X+teamMemberOwn(i).Radius):-2*(teamMemberOwn(i).Radius):(Target{i}.X-teamMemberOwn(i).Radius);
+              xStep{1,i} = (teamMemberOwn(i).Position.X+teamMemberOwn(i).Radius):-potStep:(Target{i}.X-teamMemberOwn(i).Radius);
               if (xStep{1,i}(1,end) < Target{i}.X-teamMemberOwn(i).Radius)
-                  xStep{1,i}(1,end+1) = xStep{1,i}(1,end)+2*(teamMemberOwn(i).Radius);
+                  xStep{1,i}(1,end+1) = xStep{1,i}(1,end)+potStep;
               end
-              yStep{1,i} = 0:2*(teamMemberOwn(i).Radius):Environment.yLim;
+              yStep{1,i} = 0:potStep:Environment.yLim;
           end
               
         Field = {xStep yStep};
@@ -55,11 +56,16 @@ function [potField, robotIndexes] = buildUpPotField(State, teamMemberOwn, Target
             for count = 1:length(Obstacle)
                 CurrentObst = Obstacle(count).Position.RowForm();
                 PlaceOne = ones(size(Place{i},1),size(Place{i},2));
+                OneMat = PlaceOne;
                 PlaceOne(:,1:2:end) = CurrentObst(1)*PlaceOne(:,1:2:end);
                 PlaceOne(:,2:2:end) = CurrentObst(2)*PlaceOne(:,2:2:end);
-                potObst(1,count) = (1/(abs(Vector2(Place{i}-PlaceOne))) / ((sigma)^2));
+                potObst{1,count} = ((((Place{i}-PlaceOne))) ./ ((sigma)^2));
             end
-            potObst = 2*mean(potObst);
+            Sum = zeros(size(potObst{1},1),size(potObst{1},2));
+            for cObst = 1:length(potObst)
+                Sum = Sum+potObst{cObst};
+            end
+            potObstV = abs(Sum/cObst);
         else
             potObst{1}(1,1) = nan;
             robotIndexes{1,1} = [0 0;];
@@ -70,13 +76,27 @@ function [potField, robotIndexes] = buildUpPotField(State, teamMemberOwn, Target
         PlaceOne = ones(size(Place{i},1),size(Place{i},2));
         PlaceOne(:,1:2:end) = TargetPot(1)*PlaceOne(:,1:2:end);
         PlaceOne(:,2:2:end) = TargetPot(2)*PlaceOne(:,2:2:end);
-        potFieldXY = (Place{i}-PlaceOne).^2 / (sigma^2); % + potObst;
+        potFieldXY = (Place{i}-PlaceOne).^2 / (sigma^2) + potObst;
         OszlopInt = round(size(potFieldXY,1)/2);
         potField{i} = zeros(size(potFieldXY,1),OszlopInt);
         potField{i}(:,1:size(potFieldXY,2)/2) = sqrt(potFieldXY(:,1:2:end)+potFieldXY(:,2:2:end));
+        
+        [robX, robY] = locateMyRobot(Field,teamMemberOwn(i));
+        robotIndexes{1,i} = [robX robY;];
+        
+        %--For the good pictures in the documentation--
+        %[robX, robY] = locateMyRobot(Field,teamMemberOwn(1));
+        if ~(isnan(robX))
+        potField{1,1}(robY,robX) = potField{1,1}(robY,robX)+1;
+%             for obst = 1:length(Obstacle)
+%                 [obstX, obstY] = locateMyRobot(Field,Obstacle(obst));
+%                 if ~isnan(obstX)
+%                     potField{1,1}(obstY,obstX) = potField{1,1}(obstY,obstX)+2;
+%                 end
+%             end
+        end
+        
     end
-    [robX, robY] = locateMyRobot(Field,teamMemberOwn(i));
-    robotIndexes{1,i} = [robX robY;];
 end
         
                     
